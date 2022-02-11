1ce8e9aaf0e9fe54f2af296f8c6901827c16346d
susannejung <95688233+susannejung@users.noreply.github.com>
2022-02-11 08:58:20 +0100

    Initial commit

---
+++ src/FilEksempler/Main.java
@@ -0,0 +1,40 @@
+package FilEksempler;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+       SimpleGeometricObject s=new SimpleGeometricObject();
+       System.out.print(s.getDateCreated());
+
+        // Create and initialize two objects
+        Object object1 = new CircleFromSimpleGeometricObject(1);
+        Object object2 = new RectangleFromSimpleGeometricObject(1, 1);
+
+        // Display circle and rectangle
+     //   displayObject(object1);
+     //   displayObject(object2);
+
+      //  System.out.println(object1.toString());
+
+        PolyMorphismDemo pd=new PolyMorphismDemo();
+        pd.main();
+
+    }
+
+    /** A method for displaying an object */
+    public static void displayObject(Object object) {
+        if (object instanceof CircleFromSimpleGeometricObject) {
+            System.out.println("The circle area is " +
+                    ((CircleFromSimpleGeometricObject)object).getArea());
+
+            System.out.println("The circle diameter is " +
+                    ((CircleFromSimpleGeometricObject)object).getDiameter());
+        }
+        else if (object instanceof
+                RectangleFromSimpleGeometricObject) {
+            System.out.println("The rectangle area is " +
+                    ((RectangleFromSimpleGeometricObject)object).getArea());
+        }
+    }
+    }
